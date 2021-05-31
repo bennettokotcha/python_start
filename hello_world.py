c381859eaeb243d489feb522547f38f27903b373
bennettokotcha <bennettokotcha@gmail.com>
2021-05-31 15:07:26 -0500

    started using python, 'hello world' string manipulation(interpolation)

---
+++ hello_world.py
@@ -0,0 +1,43 @@
+# print ('Hello World')
+# print ('new language means creating new pathwyas in my brain...alot of learning!!')
+# x = 'Hello Python'
+# print (x)
+# y = 42
+# print (y)
+# print (x, y)
+# print('this is a sample string')
+# name = '`Bennett` the Don!'
+# print('My name is ' + name)
+# first_name = "Bennett"
+# last_name = "Okotcha"
+# age = 28
+# print(f"My name is {first_name} {last_name} and I am {age} years old.")
+# print("My name is {} {} and I am {} years old!".format(first_name, last_name, age))
+# print("My name is %s %s, and I am %d years old" % (first_name, last_name, age))
+# x = 'bennett NDOZI okotcha'
+# print(x.lower().count('o'))
+# print(x.lower().islower())
+# print(x.upper().islower())
+# print(x.lower().split('o'))
+# print(x.endswith('a'))
+# print(x.isalnum())
+# print(x.lower().isalnum())
+
+
+# ASSIGNMENT HELLO WORLD **
+# 1 print Hello World
+print('hello world'.title())
+print ("Hello World")
+# 2 print "Hello Noelle!" with the name in the variable
+name = 'Bennett'
+print('Hello ' + name + '!')
+print ('Hello', name, '!')
+# 3 print "Hello 42!" with the number as a varialble
+number = 21
+print ('Hello ' + str(number) + '!')
+print ('Hello', number, '!')
+# 4 print "I love to eat rice and stew." with the foods in  variables
+fav_food1 = 'rice'
+fav_food1A = 'stew'
+print("I love to eat {} and {}.".format(fav_food1, fav_food1A))
+print(f"I love to eat {fav_food1} and {fav_food1A}.") 
