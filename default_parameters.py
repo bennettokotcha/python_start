795e3c989f4b0ded3804b761c2f4b5409131acd7
bennettokotcha <83544841+bennettokotcha@users.noreply.github.com>
2021-06-09 16:52:16 -0500

    Delete default_parameters.py

--- default_parameters.py
+++
@@ -1,8 +0,0 @@
-def beCheerful(name='', repeat=2): #default parameters can be optional for the caller of the function 
-    print(f'good morning {name}\n' * repeat)
-beCheerful()
-beCheerful('Bennett')
-beCheerful(name='Jospeh')
-beCheerful(repeat=6)
-beCheerful(name='Ife', repeat=4)
-beCheerful(repeat=7, name='King Okotcha')
